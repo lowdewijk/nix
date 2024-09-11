5f38f614e569d9a27ec80a032cf9645d67aef801
Lodewijk Bogaards <lodewijk.bogaards@gmail.com>
2024-09-11 11:40:12 +0200

    Add nixos-rebuild.sh shell script

---
+++ nixos-rebuild.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Check if no arguments were provided
+if [ $# -eq 0 ]; then
+  echo "No arguments provided. Run 'nixos-rebuild --help' for a full listing."
+  exit 1
+fi
+
+echo "Copying nix files from $SCRIPT_DIR to /etc/nixos"
+sudo cp -rf $SCRIPT_DIR/*.nix /etc/nixos
+sudo cp -rf $SCRIPT_DIR/flake.lock /etc/nixos
+
+sudo nixos-rebuild $@
