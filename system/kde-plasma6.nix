b9afd90a2ba23f334a772a4d071a55a1f7061661
Lodewijk Bogaards <lodewijk.bogaards@gmail.com>
2024-10-18 20:06:18 +0200

    Make sure plasma 6 uses NVIDIA on external monitor

--- system/kde-plasma6.nix
+++ system/kde-plasma6.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   # configure x11
   services.xserver = {
     enable = true;
@@ -8,14 +8,13 @@
     };
   };
 
-  # https://www.reddit.com/r/NixOS/comments/1d4l6ak/plasma_6_stuck_on_black_screen_wayland_and_nvidia/
-  boot.kernelParams = [
-    "nvidia-drm.fbdev=1"
-  ];
-
   services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [];
+  programs.xwayland.enable = true;
+
+  environment.variables = {
+    KWIN_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card2";
+  };
 }
--- system/nvidia.nix
+++ system/nvidia.nix
@@ -1,11 +1,22 @@
 {config, ...}: {
+  boot = {
+    initrd.kernelModules = ["nvidia"];
+    extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
+    # https://www.reddit.com/r/NixOS/comments/1d4l6ak/plasma_6_stuck_on_black_screen_wayland_and_nvidia/
+    kernelParams = [
+      "nvidia-drm.fbdev=1"
+    ];
+  };
+
+  hardware.opengl.enable = true;
+
   hardware = {
     graphics.enable = true;
 
     nvidia = {
       modesetting.enable = true;
       nvidiaSettings = true;
-      open = true;
+      open = false;
       package = config.boot.kernelPackages.nvidiaPackages.latest;
       powerManagement = {
         enable = false;
