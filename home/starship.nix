d2fae94e257924c554b76601c42934c9eb2eb547
Lodewijk Bogaards <lodewijk.bogaards@gmail.com>
2024-11-05 09:48:58 +0100

    Starship: replace nix icon with a text, because somehow the icon messes up my zsh

--- home/starship.nix
+++ home/starship.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{...}: {
   # starship - a customizable prompt for any shell
   programs.starship = {
     enable = true;
@@ -11,6 +7,12 @@
     settings = {
       add_newline = false;
       line_break.disabled = true;
+
+      # somehow the nix symbol messes up the spacing on my zsh
+      # and this fixes it :shrug:
+      nix_shell = {
+        symbol = "Nix ";
+      };
     };
   };
 }
