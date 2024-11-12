2972f5a5d9ffaac06149dfa7f34688c2ac2be6f5
Lodewijk Bogaards <lodewijk.bogaards@gmail.com>
2024-11-12 17:08:54 +0100

    Neovim: add <leader><leader> for telescope buffers

--- home/neovim/lua/myconfig/keybinds.lua
+++ home/neovim/lua/myconfig/keybinds.lua
@@ -66,6 +66,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 vim.keymap.set("n", "<leader>fc", "<cmd>Telescope neoclip<cr>", { desc = "Telescope help tags" })
 
