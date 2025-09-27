740d921a5d81ebcbf276081547f24edc0cae90ba
Lodewijk Bogaards <lodewijk.bogaards@gmail.com>
2025-09-27 07:27:53 +0200

    packages: add calc

--- system/packages.nix
+++ system/packages.nix
@@ -23,40 +23,35 @@
     git
     wget
     eza
-    aichat
+    aichat # prompt LLM on the command line
     devenv
     fd # used by telescope
     neofetch
     ffmpeg
-    sshfs
-
+    sshfs # mount ssh disks
+    nethogs # see what's eating up bandwidth
+    htop # better top
+    colordiff # better diff
     git-repo-sync
-
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    httpie # execute rest commands on the command line
+    file
+    which
+    ncdu # inspect diskspace
+    btop # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+    pciutils # for inspecting hardware
+    glxinfo # for checking whether X-server is running hardware or software rendering
+    lsof # list open files
+    calc # calculate on the command line
     # archives
     zip
     xz
     unzip
     p7zip
-
-    # utils
-    ripgrep # recursively searches directories for a regex pattern
-    jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processor https://github.com/mikefarah/yq
-    httpie # execute rest commands on the command line
-
-    # misc
-    file
-    which
-    ncdu # inspect diskspace
-
-    btop # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
-
-    pciutils # for inspecting hardware
-    glxinfo # for checking whether X-server is running hardware or software rendering
-
-    lsof # list open files
   ];
 in {
   environment.systemPackages = shellPackages ++ desktopPackages;
