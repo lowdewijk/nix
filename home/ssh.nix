697533c9147611fa095e94dde338a4ad70dfd5bc
Lodewijk Bogaards <lodewijk.bogaards@gmail.com>
2025-11-20 17:06:16 +0100

    ssh: fix deprecation warning

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,7 +1,7 @@
 {
   programs.ssh = {
     enable = true;
-
+    enableDefaultConfig = false;
     matchBlocks."*" = {
       serverAliveInterval = 60;
       identityAgent = "~/.1password/agent.sock";
