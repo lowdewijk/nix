2a8647bf851fa9ab32a9e4a58a4a8734711eacc5
Lodewijk Bogaards <lodewijk.bogaards@gmail.com>
2026-07-03 09:44:25 +0200

    firefox: remove pywalfox

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,14 +1,4 @@
 {
-  pkgs,
-  lib,
-  ...
-}: {
-  home.packages = [pkgs.pywalfox-native];
-
-  home.activation.installPywalfoxManifest = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${lib.getExe pkgs.pywalfox-native} install
-  '';
-
   programs.firefox = {
     enable = true;
     configPath = ".mozilla/firefox";
@@ -16,10 +6,6 @@
       ExtensionSettings = {
         "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "pywalfox@frewacom.org" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/pywalfox/latest.xpi";
           installation_mode = "force_installed";
         };
         "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
