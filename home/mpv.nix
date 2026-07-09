55159672f4f42bb7072469bc1bc5db27dcd8dd6b
Lodewijk Bogaards <lodewijk.bogaards@gmail.com>
2026-07-09 17:25:38 +0200

    mpv: add some keybinds for next and previous file and showing the filanem and enable autoloop

--- home/mpv.nix
+++ home/mpv.nix
@@ -2,7 +2,15 @@
   programs.mpv = {
     enable = true;
     config = {
+      autocreate-playlist = "filter";
       keep-open = "always";
+      loop-file = "inf";
+      osd-font-size = 14;
+    };
+    bindings = {
+      h = "playlist-prev";
+      l = "playlist-next";
+      "f" = ''show-text "''${osd-ass-cc/0}{\\an8}''${filename}"'';
     };
   };
 }
