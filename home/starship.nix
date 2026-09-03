5927f86345bb28aac56c2c608768920bea86d537
Lodewijk Bogaards <lodewijk.bogaards@gmail.com>
2026-09-03 15:23:37 +0200

    starship: place prompt on new line

--- home/starship.nix
+++ home/starship.nix
@@ -5,9 +5,6 @@
     enableZshIntegration = true;
     enableNushellIntegration = true;
     settings = {
-      add_newline = false;
-      line_break.disabled = true;
-
       # somehow the nix symbol messes up the spacing on my zsh
       # and this fixes it :shrug:
       nix_shell = {
